b8e4e34da08cdb365a7deea09452f525bc227369
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-07-25 15:59:25 +0530

    security

--- src/main/java/com/oauth2/springsocial/config/SecurityConfig.java
+++ src/main/java/com/oauth2/springsocial/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.oauth2.springsocial.config;
 
+import com.oauth2.springsocial.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
