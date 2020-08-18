f6519a7c7886432b164fa5a3db9f9d34e6953b2d
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-08-18 18:30:48 +0530

    OAuth2AuthenticationProcessingException

--- src/main/java/com/oauth2/springsocial/exception/OAuth2AuthenticationProcessingException.java
+++ src/main/java/com/oauth2/springsocial/exception/OAuth2AuthenticationProcessingException.java
@@ -1,4 +1,8 @@
 package com.oauth2.springsocial.exception;
 
 public class OAuth2AuthenticationProcessingException {
+
+    public OAuth2AuthenticationProcessingException(String msg, Throwable t) {
+        super(msg, t);
+    }
 }
